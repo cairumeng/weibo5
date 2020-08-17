411f6a91ae0b680410c838714750bc9bd6f11b49
cairumeng <cairumeng@gmail.com>
2020-08-17 15:55:15 +0200

    auth controller

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Route::get('users/activate/{token}', 'UsersController@activate')->name('users.ac
 Route::get('sessions', 'SessionsController@create')->name('sessions.create');
 Route::post('sessions', 'SessionsController@store')->name('sessions.store');
 Route::delete('sessions', 'SessionsController@destroy')->name('sessions.destroy');
+
+
+Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'ResetPasswordController@update');
